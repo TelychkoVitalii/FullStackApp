c494869bb1ba8f151220a35970134d4edf578617
Vitaly Telichko <vitaly.telichko@dataart.com>
2017-12-14 15:14:55 +0200

    initial two

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/nodekb');
 const db = mongoose.connection;
@@ -25,6 +26,15 @@ const Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Body parse middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Body parse application/json
+app.use(bodyParser.json());
+
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Home Route
 app.get('/', function (req, res) {
     Article.find({}, function (err, articles) {
@@ -39,11 +49,35 @@ app.get('/', function (req, res) {
     });
 });
 
+// Get Single Article
+app.get('/article/:id', function (req, res) {
+    Article.findById(req.params.id, function (err, article) {
+        res.render('article', {
+            article: article
+        });
+    });
+});
+
 // Add Route
 app.get('/articles/add', function (req, res) {
     res.render('add_article', {
         title: 'Add Article'
     });
+});
+
+// Add Submit POST Route
+app.post('/articles/add', function (req, res) {
+   const article = new Article();
+   article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+    article.save(function (err) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect('/');
+        }
+    })
 });
 
 // Start Server
